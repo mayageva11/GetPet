7697e1090e415c70efa18c1a97439c618ed5de2d
Shahar Wissbecker <shahar939@gmail.com>
2024-01-17 13:45:19 +0200

    xml for my upload

---
+++ app/src/main/java/com/getpet/activities/MyUploadsActivity.kt
@@ -0,0 +1,12 @@
+package com.getpet.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.getpet.R
+
+class MyUploadsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_uploads)
+    }
+}
